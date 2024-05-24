bcd8a8f376138443a74665419c17e4a559b38ff2
salma0123 <salmaesfidani972@gmail.com>
2024-05-24 13:59:44 +0100

    profile page

---
+++ src/main/java/com/example/demo1/HistoryPage.java
@@ -0,0 +1,25 @@
+package com.example.demo1;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+
+public class HistoryPage {
+
+    @FXML
+    private Button btnHome;
+
+    @FXML
+    private Label lblHistory;
+
+    @FXML
+    private ListView<?> lvHistory;
+
+    @FXML
+    void PbtnHome(ActionEvent event) {
+
+    }
+
+}
